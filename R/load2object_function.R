bdf1188afb8662f80c54902a02185db17870b44b
FunGeST <30511199+FunGeST@users.noreply.github.com>
2019-06-12 16:44:46 +0200

    Delete load2object_function.R

--- R/load2object_function.R
+++
@@ -1,8 +0,0 @@
-load2object <- function (filename) 
-{
-  if (file.exists(filename)) 
-    return(eval(parse(text = load(filename))))
-  cat(paste("error! the file ", filename, 
-            " was not found :("))
-  NULL
-}
